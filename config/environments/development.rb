796322fee96d09c576f3ef3e57b3dfcaefbf0621
Gurjot <gurjot.singh@bluebash.co>
2023-03-21 18:09:58 +0530

    update local host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # #LiveReloader
   config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'videoalbum.onrender.com', port: 3000 }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
