4ee5781246dddae1312330c167aa93959624303f
Gurjot-Singh-Maan <128450185+Gurjot-Singh-Maan@users.noreply.github.com>
2023-03-21 11:53:43 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,6 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
   
-    config.secret_key_base = ENV['SECRET_KEY_BASE']
 
 
   # Don't log any deprecations.
